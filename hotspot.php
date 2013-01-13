3951c63ed98b2849e29ae90299d360cbe1cc1a14
dpowney <dpowney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-13 04:27:02 +0000

    Changed description
    git-svn-id: https://plugins.svn.wordpress.org/hotspots/trunk@651817 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hotspot.php
+++ hotspot.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: HotSpots
-Plugin URI: www.danielpowney.com/hotspots.zip
+Plugin URI: http://wordpress.org/extend/plugins/hotspots/
 Description: HotSpots is a simple plugin which draws a heat map of mouse clicks overlayed on your webpage allowing you to improve usability by analysing user behaviour.
 Version: 1.2
 Author: Daniel Powney
