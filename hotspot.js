bd80589c4962a567978dfcabd6bcb2f23dbb7daf
dpowney <dpowney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-13 06:12:40 +0000

    Fixed params bug
    git-svn-id: https://plugins.svn.wordpress.org/hotspots/trunk@651863 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hotspot.js
+++ hotspot.js
@@ -50,11 +50,13 @@ var urlHelper = new function() {
 	 */
 	this.getUrlParams = function(url) {
 		var params = [], hash;
-		var hashes = url.slice(url.indexOf('?') + 1).split('&');
-		for ( var i = 0; i < hashes.length; i++) {
-			hash = hashes[i].split('=');
-			params.push(hash[0]);
-			params[hash[0]] = hash[1];
+		if (url.indexOf("?") !== -1) {
+			var hashes = url.slice(url.indexOf('?') + 1).split('&');
+			for ( var i = 0; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				params.push(hash[0]);
+				params[hash[0]] = hash[1];
+			}
 		}
 		return params;
 	};
